3645045e6ccc38ab3bd08e3fe253dcfa38dcdfcc
Martin Bombara <martinb@lagash.com>
2018-12-21 16:52:47 -0300

    changed atributtes names

--- app/src/main/java/com/martinb/marvelapp/data/model/ComicsData.kt
+++ app/src/main/java/com/martinb/marvelapp/data/model/ComicsData.kt
@@ -5,4 +5,4 @@ import com.squareup.moshi.Json
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
-data class ComicsData(@Json(name="results")val results: List<ComicInformation>) : Parcelable
+data class ComicsData(@field:Json(name="results")val comicInformation: List<ComicInformation>) : Parcelable
--- app/src/main/java/com/martinb/marvelapp/data/model/ComicsResults.kt
+++ app/src/main/java/com/martinb/marvelapp/data/model/ComicsResults.kt
@@ -7,4 +7,4 @@ import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 data class ComicsResults(val code: String, val status: String, val copyright: String,
-                         val attributionText: String, val attributtionHTML: String, @Json(name="data") val data: ComicsData, val etag: String) : Parcelable
+                         val attributionText: String, val attributtionHTML: String, @field:Json(name="data") val data: ComicsData, val etag: String) : Parcelable
